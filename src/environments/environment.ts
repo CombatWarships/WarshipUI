89cbceb81f44cd17b7244270ac6c8949b2cfd04b
Justin Scott <justin@scott.org>
2023-11-24 20:19:39 -0600

    aks service name changed.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   warshipSearchUrl: 'http://20.118.83.236/warshipregistry',
-  warshipImportUrl: 'http://20.118.83.236/warship-import'
+  warshipImportUrl: 'http://20.118.83.236/warshipimport'
 };
